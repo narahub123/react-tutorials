497d00582e1428ccd00a4d0a7d39e6f1ca03e8ff
nara <parknara0213@gmail.com>
2024-02-07 15:03:33 +0900

    240207

--- 07-refs-and-portal-project/src/components/TimerChallenge.jsx
+++ 07-refs-and-portal-project/src/components/TimerChallenge.jsx
@@ -1,12 +1,42 @@
+import { useRef, useState } from "react";
+
+// a variable that will shared across all component instances that are based on the component function
+// let timer;
+
 export default function TimerChallenge({ title, targetTime }) {
+  // every component instance of the TimerChallenge component will get its own timer ref that works totally independent
+  const timer = useRef();
+
+  const [timeStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  // a function that stop timer
+  // need the timer access stop function
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
-      <button>Start Challenge</button>
-      <p className="">Time is running... / Timer inactive</p>
+      <button onClick={timeStarted ? handleStop : handleStart}>
+        {timeStarted ? "Stop" : "Start"} Challenge
+      </button>
+      <p className={timeStarted ? "active" : undefined}>
+        {timeStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 } // TimerChallenge() ends
