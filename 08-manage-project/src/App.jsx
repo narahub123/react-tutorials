f1b809af8a41042f73c864b8abe95452621c7866
nara <parknara0213@gmail.com>
2024-02-08 13:46:32 +0900

    240208

--- 08-manage-project/src/App.jsx
+++ 08-manage-project/src/App.jsx
@@ -1,9 +1,12 @@
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NoProjectSelected />
     </main>
   );
 }
