08ab1e09eb73051d8d30c4ef30db5763997af461
nara <parknara0213@gmail.com>
2024-02-08 18:08:15 +0900

    240208

--- 08-manage-project/src/App.jsx
+++ 08-manage-project/src/App.jsx
@@ -25,7 +25,15 @@ function App() {
       };
     });
   }
-  function handleDeleteTask() {}
+
+  function handleDeleteTask(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
 
   function handleSelectProject(id) {
     setProjectState((prevState) => {
