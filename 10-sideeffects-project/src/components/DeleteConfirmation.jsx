04474f1353223fc121d75e87063b4e4c7e9352b4
nara <parknara0213@gmail.com>
2024-02-10 18:14:09 +0900

    240210

--- 10-sideeffects-project/src/components/DeleteConfirmation.jsx
+++ 10-sideeffects-project/src/components/DeleteConfirmation.jsx
@@ -1,21 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
 
 const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remaingTime, setRemainingTime] = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log("INTERVAL");
-      setRemainingTime((prevTime) => prevTime - 10);
-    }, 10);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   useEffect(() => {
     console.log("TIMER SET");
 
@@ -42,8 +30,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      {/* re-render the progress bar frequently */}
-      <progress value={remaingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
