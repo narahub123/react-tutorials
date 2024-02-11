26282f70c872f98a6afa3fe765bc945979b7ecee
nara <parknara0213@gmail.com>
2024-02-11 12:24:52 +0900

    240211 output questions and register answers

--- 11-quizapp-project/src/components/Quiz.jsx
+++ 11-quizapp-project/src/components/Quiz.jsx
@@ -3,9 +3,34 @@
 // store answers of user
 import { useState } from "react";
 
-export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0); // currently active question from question array
-  const [userAnswers, setUserAnswers] = useState([]); // register user's answer
+import QUESTIONS from "../questions.js";
 
-  return <p>Currently active Question</p>;
+export default function Quiz() {
+  // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0); redundent!!
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  // derived state : computed value
+  const activeQuestionIndex = userAnswers.length;
+
+  // store selected answer in userAnswers array
+  function handleSelectAnswer(selectedAnswer) {
+    // update state based on previous state
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            {/* passing an argument */}
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
