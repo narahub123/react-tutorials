cc2a34456e6bd2007272c5007614b22683b97b1b
nara <parknara0213@gmail.com>
2024-02-11 12:32:45 +0900

    240211 shuffle answers

--- 11-quizapp-project/src/components/Quiz.jsx
+++ 11-quizapp-project/src/components/Quiz.jsx
@@ -1,17 +1,16 @@
-// what currently active question is
-// change it to different question whenever user answer the question : useState
-// store answers of user
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
-  // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0); redundent!!
   const [userAnswers, setUserAnswers] = useState([]);
 
   // derived state : computed value
   const activeQuestionIndex = userAnswers.length;
 
+  // shuffle
+  const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5); // shuffling
   // store selected answer in userAnswers array
   function handleSelectAnswer(selectedAnswer) {
     // update state based on previous state
@@ -24,7 +23,7 @@ export default function Quiz() {
     <div id="question">
       <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
       <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+        {shuffleAnswers.map((answer) => (
           <li key={answer} className="answer">
             {/* passing an argument */}
             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
