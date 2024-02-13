55d5e68e8661ce2f1294e0bb77c4d2953c3af130
nara <parknara0213@gmail.com>
2024-02-13 15:57:31 +0900

    240213 using fetch()

--- 14-http-project/src/components/AvailablePlaces.jsx
+++ 14-http-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,25 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+import Places from "./Places.jsx";
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // a function that can be used to send http request to some other servers
+  // the code executes whenever component function executes => may cause infinite loop
+  fetch("http://localhost:3000/place")
+    .then((response) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places); // it causes the component function executes again
+    });
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
