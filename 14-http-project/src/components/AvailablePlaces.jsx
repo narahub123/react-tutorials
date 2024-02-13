d39691f96f8b90dad8377737a0a90ac534ab764a
nara <parknara0213@gmail.com>
2024-02-13 16:59:57 +0900

    240213 data transform

--- 14-http-project/src/components/AvailablePlaces.jsx
+++ 14-http-project/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false); // loading state
@@ -20,15 +21,26 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Failed to fetch places");
         }
 
-        setAvailablePlaces(resData.places);
+        // need to fetch user's location before set available places to calculate the distance of places
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+
+          setAvailablePlaces(resData.places);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || "Could not fetch places, please try again later",
         });
+        setIsFetching(false);
       }
 
-      setIsFetching(false);
+      // setIsFetching(false);
     }
 
     fetchPlaces();
