048dd9e1b9d9444925eec67269ca139aac79a272
nara <parknara0213@gmail.com>
2024-02-14 12:01:36 +0900

    240214 mixing custom and built-in validation

--- 16-form-project/src/components/Signup.jsx
+++ 16-form-project/src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState();
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,6 +10,12 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if (data.password !== data["confirm-password"]) {
+      setPasswordsAreNotEqual(true);
+
+      return;
+    }
 
     console.log(data);
   }
@@ -39,6 +49,9 @@ export default function Signup() {
             type="password"
             name="confirm-password"
           />
+        </div>
+        <div className="control-error">
+          {passwordsAreNotEqual && <p>Password must match.</p>}
         </div>
       </div>
 
