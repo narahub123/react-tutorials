9827928813d70791e85244e63fd90d3162a19654
nara <parknara0213@gmail.com>
2024-02-15 13:33:25 +0900

    240215 create a Cart component and open cart modal

---
+++ 17-food-order-project/src/components/Cart.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import Button from "../components/UI/Button";
+import { currencyFormatter } from "../util/formatting";
+import UserProgressContext from "../store/UserProgressContext";
+
+// output cart data
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
