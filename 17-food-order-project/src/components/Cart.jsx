88b70c03d2a9e21e213db247d057abf474058f5a
nara <parknara0213@gmail.com>
2024-02-15 13:40:57 +0900

    240215 close cart modal

--- 17-food-order-project/src/components/Cart.jsx
+++ 17-food-order-project/src/components/Cart.jsx
@@ -15,6 +15,9 @@ export default function Cart() {
     0
   );
 
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
   return (
     <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
@@ -27,8 +30,10 @@ export default function Cart() {
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close</Button>
-        <Button>Go to Checkout</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
   );
--- 17-food-order-project/src/components/UI/Modal.jsx
+++ 17-food-order-project/src/components/UI/Modal.jsx
@@ -1,27 +1,21 @@
-// use modal component from anywhere component tree but always inject dialog when it's visible
-// in speific area of real dom that developer control upfront
-
 import { useEffect, useRef } from "react";
-// 1. import createPortal
+
 import { createPortal } from "react-dom";
 
 export default function Modal({ children, open, className = "" }) {
-  // 3. open prop controls if the dialog is open or not
   const dialog = useRef();
 
   useEffect(() => {
+    const modal = dialog.current;
+
     if (open) {
-      dialog.current.showModal();
+      modal.showModal();
     }
+
+    return () => modal.close();
   }, [open]);
 
-  // 2. create portal createPortal(rendered component, where the component is rendered)
   return createPortal(
-    // 4. control open prop
-    // 4-1 built property make grey area behind the modal and that protect interacting whilst the dialog is open
-    // <dialog open={open}>
-    // 4-2 forwordRef()
-    // 4-3 useEffect() and useRef()
     <dialog ref={dialog} className={`modal ${className}`}>
       {children}
     </dialog>,
