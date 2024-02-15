2d48460c3c620056df7123d95d6cedd100dbb294
nara <parknara0213@gmail.com>
2024-02-15 12:35:17 +0900

    240215 create cart context

--- 17-food-order-project/src/components/MealItem.jsx
+++ 17-food-order-project/src/components/MealItem.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
 // price formatter
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
   return (
     <li className="meal-item">
       <article>
@@ -15,7 +23,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
---
+++ 17-food-order-project/src/store/CartContext.jsx
@@ -0,0 +1,108 @@
+// manage cart data and cart context
+// 1. spread data into all components
+import { createContext, useReducer } from "react";
+
+// 2. default value of context
+// plan of context in advance
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+// 5. reducer function
+// return updated state
+// actioin : how to update state
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    // ... update the state to add a meal item
+    // state.items.push(action.item);
+    // shouldn't mutate existing state like this
+    // bcoz push will add existing state => change the memory before cartReducer executes
+    // not all the time a new item added
+
+    // check if we already have the item in item array
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    // if the item already exists
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      // update quantity property
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    // if there is not item in items array
+    else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    // ... remove an item from the state
+    // update items in immutable way
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    // check the quantity of the item
+
+    const updatedItems = [...state.items];
+    // if quantity is equal to 1, remove the item
+    if (existingCartItem.quantity === 1) {
+      // remove an item at the index
+      updatedItems.splice(existingCartItemIndex, 1);
+    } // if quantity is bigger than 1, reduce the quantity
+    else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+}
+
+// 3. wrap around components and make context available to them and actural data management and state management
+export function CartContextProvider({ children }) {
+  // 4. manage state and make complex state simpler and make it easy to move state management logic out of the functoin
+  // 6. connect cart logic to different components
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] }); // initial state value
+
+  // 7. pass cart context state to cartContext provider component
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+  console.log(cartContext);
+
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+}
+
+export default CartContext;
