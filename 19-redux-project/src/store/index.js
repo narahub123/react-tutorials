84a81a9ab8db8043f88048d79a5abf9e4dd860cb
nara <parknara0213@gmail.com>
2024-03-09 12:48:35 +0900

    240309 add a state slice

--- 19-redux-project/src/store/index.js
+++ 19-redux-project/src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++; // automatically clone the existing state -> immutable
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      // need some data that's attached to the action
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  }, // a object of all the reducers the slice needs
+}); // preparing a slice of global state
 
 // create reducer function
 const counterReducer = (state = initialState, action) => {
