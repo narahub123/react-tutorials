d53ea2f9de51f36ef843bdcea49be6ca0e82cfd0
nara <parknara0213@gmail.com>
2024-03-10 22:47:51 +0900

    240310 use action creator

--- 20-advanced-redux-project/src/store/cart-slice.js
+++ 20-advanced-redux-project/src/store/cart-slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { uiActions } from "./ui-slice";
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
@@ -37,6 +39,58 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// 1.1 create an anction creator
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    // 1.2. dispatch action
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    // 1.5 a function for try-catch block
+    const sendRequest = async () => {
+      // 1.3 fetch data
+      const response = await fetch(
+        "https://realtime-demo-51a6e-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    // 1.6 try-catch block
+    try {
+      await sendRequest();
+      // 1.4 success state
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      // 1.7 error state
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
