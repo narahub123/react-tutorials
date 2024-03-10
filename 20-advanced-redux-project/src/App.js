07626d97561f37c2139c9c429857ce6163014eef
nara <parknara0213@gmail.com>
2024-03-10 22:20:53 +0900

    240310 side-effect logic in component

--- 20-advanced-redux-project/src/App.js
+++ 20-advanced-redux-project/src/App.js
@@ -1,28 +1,93 @@
-import { useSelector } from "react-redux"; // extract data from Redux
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux"; // useSelector : extract data from Redux
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice"; // for dispatch
+import Notification from "./components/UI/Notification";
 
-// render Cart component conditionally based on the UI slice state value
+// 5.1 avoid sending cart when it runs for the first time
+let isInitial = true;
 
 function App() {
+  // 3.1 dispatch show notification action
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  // 4.1use notification state
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch("https://realtime-demo-51a6e-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    // 1.1 asynchronous operation to get response
+    const sendCartData = async () => {
+      // 3.2 loading state notification
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+
+      const response = await fetch(
+        "https://realtime-demo-51a6e-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      // 1.2 validation
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      // 3.3 success state notification
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    // 5.2 prevent sending data for the first time
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    // the reason why the error code out of sendata is due to handling all kind of errors
+    // 3.4 error state notification
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
     });
-  }, [cart]);
+    // 3.5 add dependency
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    // 4.2 add notification component to use it conditionally
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- 20-advanced-redux-project/src/store/ui-slice.js
+++ 20-advanced-redux-project/src/store/ui-slice.js
@@ -2,10 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false },
+  // 2.1 add a new state for notification
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible; // it looks like mutable code but immur translates immutable code and create a new state object
+    },
+    // 2.2 add a new reducer for notification
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status, // pending, success, error
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
