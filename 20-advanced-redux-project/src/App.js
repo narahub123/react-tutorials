fce4d333e3b0814f1d453b840f792d0624dcfafc
nara <parknara0213@gmail.com>
2024-03-10 19:18:45 +0900

    240310 useEffect in a component

--- 20-advanced-redux-project/src/App.js
+++ 20-advanced-redux-project/src/App.js
@@ -3,11 +3,21 @@ import { useSelector } from "react-redux"; // extract data from Redux
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 // render Cart component conditionally based on the UI slice state value
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://realtime-demo-51a6e-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
