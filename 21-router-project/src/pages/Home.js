dad4a6cfd7420797f87cd569fd4cca100b58bd03
nara <parknara0213@gmail.com>
2024-03-11 18:35:28 +0900

    240311 navigating programmatically

--- 21-router-project/src/pages/Home.js
+++ 21-router-project/src/pages/Home.js
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate(); // trigger a navigation action to switch to different route from inside the code
+
+  function navigationHandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>HomePage</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigationHandler}>Navigate</button>
       </p>
     </>
   );
