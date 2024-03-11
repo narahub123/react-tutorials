fe97c5a53243d5f1a529fa345833d32c7817e03b
nara <parknara0213@gmail.com>
2024-03-11 10:05:52 +0900

    240311 define a route

--- 21-router-project/src/App.js
+++ 21-router-project/src/App.js
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+// https://example.com/ : domain
+// /product : path
+
+// define a route
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />, // the path is active, the element should be loaded
+  }, // route object
+  {},
+]);
+
 function App() {
-  return <div></div>;
+  // how to use a route
+  return <RouterProvider router={router} />;
 }
 
 export default App;
