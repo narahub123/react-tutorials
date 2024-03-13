a87695b1576fe142364375c08956ce1aa6679e3a
nara <parknara0213@gmail.com>
2024-03-14 00:36:40 +0900

    defer data fetching

--- 21-1-adv-starting-project/frontend/src/pages/EventsPage.js
+++ 21-1-adv-starting-project/frontend/src/pages/EventsPage.js
@@ -1,26 +1,24 @@
-import { useLoaderData, json } from "react-router-dom";
+import { Suspense } from "react";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
-
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
-
-  const events = data.events;
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    // show fallback whilist we're waiting for events to be fetched
+    <Suspense fallback={<p style={{ textAlign: "center" }}>loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -29,8 +27,16 @@ export async function loader() {
       {
         status: 500,
       }
-    ); // a function that create a response object that includes data in JSON format
+    );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+// load and render a component even though the future value isn't there yet
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
