904a3811c95de210e6f3e9163cf3340b8cea222d
nara <parknara0213@gmail.com>
2024-03-13 20:20:10 +0900

    240313 create a generic error page

--- 21-1-adv-starting-project/frontend/src/pages/ErrorPage.js
+++ 21-1-adv-starting-project/frontend/src/pages/ErrorPage.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
+
 import PageContent from "../components/PageContent";
+import MainNavigation from "../components/MainNavigation";
+
 const ErrorPage = () => {
+  const error = useRouteError(); // give an error object
+  // the shape of the object now depends on whether you threw a response
+  // or any other kind of object or data
+
+  // error.stauts; // in the case of throwing response
+
+  // generic error handling code
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not Found!";
+    message = "Could not find resource or page";
+  }
+
   return (
-    <PageContent title="An error occurred">
-      <p>Something went wrong</p>
-    </PageContent>
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
   );
 };
 
--- 21-1-adv-starting-project/frontend/src/pages/EventsPage.js
+++ 21-1-adv-starting-project/frontend/src/pages/EventsPage.js
@@ -24,11 +24,10 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: "Could not fetch events" };
-    throw { message: "Could not fetch events" };
+    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+      status: 500,
+    }); // for differentiating between errors
   } else {
-    // const resData = await response.json();
-
     return response;
   }
 }
