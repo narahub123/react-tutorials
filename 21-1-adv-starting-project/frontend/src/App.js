2e7cd284c1b7c0bc113fb891c4f2d0b7b6644d51
nara <parknara0213@gmail.com>
2024-03-13 12:59:23 +0900

    240313 create root route

--- 21-1-adv-starting-project/frontend/src/App.js
+++ 21-1-adv-starting-project/frontend/src/App.js
@@ -5,28 +5,35 @@ import EventsPage from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
+import Root from "./pages/Root";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/events",
-      element: <EventsPage />,
-    },
-    {
-      path: "/events/:eventId",
-      element: <EventDetailPage />,
-    },
-    {
-      path: "/events/new",
-      element: <NewEventPage />,
-    },
-    {
-      path: "/events/:eventId/edit",
-      element: <EditEventPage />,
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        }, // turn into index route
+        {
+          path: "events",
+          element: <EventsPage />,
+        },
+        {
+          path: "events/:eventId",
+          element: <EventDetailPage />,
+        },
+        {
+          path: "events/new",
+          element: <NewEventPage />,
+        },
+        {
+          path: "events/:eventId/edit",
+          element: <EditEventPage />,
+        },
+      ],
     },
   ]);
 
