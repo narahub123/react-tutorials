8f271e87fcac1fdf5707ccf4634bd710f19719d0
nara <parknara0213@gmail.com>
2024-03-13 21:12:25 +0900

    240313 access data from other routes

--- 21-1-adv-starting-project/frontend/src/pages/EditEventPage.js
+++ 21-1-adv-starting-project/frontend/src/pages/EditEventPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useRouteLoaderData } from "react-router-dom";
+
+import EventForm from "../components/EventForm";
 
 const EditEventPage = () => {
-  return <h1>Edit Event Page</h1>;
+  const data = useRouteLoaderData("event-detail");
+
+  return <EventForm event={data.event} />;
 };
 
 export default EditEventPage;
