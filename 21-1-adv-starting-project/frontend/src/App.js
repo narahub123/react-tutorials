538bfc28f0c7e72ea64d9a1ac6338609200323ff
nara <parknara0213@gmail.com>
2024-03-13 12:51:19 +0900

    240313 create router

--- 21-1-adv-starting-project/frontend/src/App.js
+++ 21-1-adv-starting-project/frontend/src/App.js
@@ -1,6 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import EventsPage from "./pages/EventsPage";
+import EventDetailPage from "./pages/EventDetailPage";
+import NewEventPage from "./pages/NewEventPage";
+import EditEventPage from "./pages/EditEventPage";
 
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/events",
+      element: <EventsPage />,
+    },
+    {
+      path: "/events/:eventId",
+      element: <EventDetailPage />,
+    },
+    {
+      path: "/events/new",
+      element: <NewEventPage />,
+    },
+    {
+      path: "/events/:eventId/edit",
+      element: <EditEventPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
