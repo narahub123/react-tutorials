664e0c6c456dc8b95b04e4fbe318c6a1e9e14181
nara <parknara0213@gmail.com>
2024-03-13 16:21:08 +0900

    240313 useNavigation()

--- 21-1-adv-starting-project/frontend/src/pages/Root.js
+++ 21-1-adv-starting-project/frontend/src/pages/Root.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
 const Root = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
