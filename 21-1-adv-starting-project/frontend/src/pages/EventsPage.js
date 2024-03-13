50efee0a266516e05b4ac4f0f94abbc34d0497b3
nara <parknara0213@gmail.com>
2024-03-13 13:16:59 +0900

    240313 output a list of dummy events to the EventsPage

--- 21-1-adv-starting-project/frontend/src/pages/EventsPage.js
+++ 21-1-adv-starting-project/frontend/src/pages/EventsPage.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "some event",
+  },
+  {
+    id: "e2",
+    title: "Another event",
+  },
+];
 
 const EventsPage = () => {
-  return <h1>Event Page</h1>;
+  return (
+    <>
+      <h1>Event Page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default EventsPage;
