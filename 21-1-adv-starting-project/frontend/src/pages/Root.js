90945e431ba41edf052988f47cfcc0dc881eab1f
nara <parknara0213@gmail.com>
2024-03-13 16:34:00 +0900

    240313 return response in loaders

--- 21-1-adv-starting-project/frontend/src/pages/Root.js
+++ 21-1-adv-starting-project/frontend/src/pages/Root.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
 const Root = () => {
-  const navigation = useNavigation();
-
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
