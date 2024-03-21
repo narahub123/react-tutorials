ab42009196670ee048f2921f720240c327b18906
nara <parknara0213@gmail.com>
2024-03-21 10:29:33 +0900

    240321 Change modes with query parameters

--- 22-authentication-project/frontend/src/components/MainNavigation.js
+++ 22-authentication-project/frontend/src/components/MainNavigation.js
@@ -41,7 +41,7 @@ function MainNavigation() {
           </li>
           <li>
             <NavLink
-              to="/auth"
+              to="/auth?mode=login" // set login mode by default
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
