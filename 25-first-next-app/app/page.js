e76b556af7d9117bf5c4ad1c4ed2f325c104b69e
nara <parknara0213@gmail.com>
2024-03-25 09:31:14 +0900

    240325 add a new page

--- 25-first-next-app/app/page.js
+++ 25-first-next-app/app/page.js
@@ -1,4 +1,5 @@
 export default function Home() {
+  // console.log("Executing"); // can see the log on the terminal not console 
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
