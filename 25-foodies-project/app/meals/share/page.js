02ddd0a62c2537838dacf199786fb1c83d3392f6
nara <parknara0213@gmail.com>
2024-03-26 16:05:05 +0900

    240326 server action for form submission

--- 25-foodies-project/app/meals/share/page.js
+++ 25-foodies-project/app/meals/share/page.js
@@ -2,6 +2,22 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  // server action
+  async function shareMeal(formData) {
+    "use server"; // create server action
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,9 +27,9 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
-            <p>
+            <p>``
               <label htmlFor="name">Your name</label>
               <input type="text" id="name" name="name" required />
             </p>
@@ -39,7 +55,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
