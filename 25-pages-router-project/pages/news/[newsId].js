b57ec0ab7741c222578e9817c90d818f0268baba
nara <parknara0213@gmail.com>
2024-03-27 15:35:29 +0900

    240327 extract dynamic parameter values

--- 25-pages-router-project/pages/news/[newsId].js
+++ 25-pages-router-project/pages/news/[newsId].js
@@ -1,5 +1,14 @@
+import { useRouter } from "next/router";
 
 function DetailPage() {
+  const router = useRouter();
+
+  // console.log(router.query.newsId);
+  const newId = router.query.newsId;
+
+  // send a request to the backend API
+  // to fetch the news item with newsId
+
   return <h1>the Detail Page</h1>;
 }
 
