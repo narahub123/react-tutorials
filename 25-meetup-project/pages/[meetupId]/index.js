f1513905889de71ab655c7cf9650037245db8ac5
nara <parknara0213@gmail.com>
2024-03-28 14:05:00 +0900

    240328 params for SSG data fetching

--- 25-meetup-project/pages/[meetupId]/index.js
+++ 25-meetup-project/pages/[meetupId]/index.js
@@ -11,4 +11,25 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
