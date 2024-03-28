04941c324f955052389479a623dda305d63a1517
nara <parknara0213@gmail.com>
2024-03-28 14:41:24 +0900

    240328 conect to mongodb

--- 25-meetup-project/pages/api/new-meetup.js
+++ 25-meetup-project/pages/api/new-meetup.js
@@ -1,17 +1,27 @@
-
-
-// define functions which contains server-side code
-// API routes only run on the server
+import { MongoClient } from "mongodb";
 
 // /api/new-meetup
 // POST
-function hander(req, res) {
+async function hander(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
 
+    const client = await MongoClient.connect(
+      "mongodb+srv://john:1234@nodeexpressproject.viwrjth.mongodb.net/meetups?retryWrites=true&w=majority&appName=NodeExpressProject"
+    );
+    const db = client.db();
 
+    const meetupCollection = db.collection("meetups");
+
+    const result = await meetupCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
 
